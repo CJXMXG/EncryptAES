4b7376e0ce07af04d5c08c2352a3d9221fefc0ff
陈文辉 <3418706125@qq.com>
2019-04-25 16:23:51 +0800

    AES加密

---
+++ EncryptAES/EncryptAES/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace EncryptAES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "加密：";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "解密：";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "json字符串:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "json字符串:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "json字符串:";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(187, 140);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(497, 21);
+            this.tb2.TabIndex = 5;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(187, 197);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(497, 21);
+            this.tb3.TabIndex = 6;
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(187, 84);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(497, 21);
+            this.tb1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 281);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb1;
+    }
+}
+
---
+++ EncryptAES/EncryptAES/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptAES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string key = "kkkkkkk123456789";//保持16位
+        //加密
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            byte[] contentByte = System.Text.Encoding.Default.GetBytes(tb1.Text.Trim());
+            byte[] resultByte = AESHelper.Encrypt(contentByte, key);//加密后
+            tb2.Text = Convert.ToBase64String(resultByte);
+        }
+
+        //解密
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] javaBaseArray = Convert.FromBase64String(tb2.Text);
+
+            byte[] finalByte = AESHelper.Decrypt(javaBaseArray, key);
+            tb3.Text = System.Text.Encoding.Default.GetString(finalByte);
+        }
+    }
+}
